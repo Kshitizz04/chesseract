6571667e7fafa240b4d9a2bb985999470b3cf4e2
Kshitizz04 <kshitizs2008@gmail.com>
2025-05-14 11:47:33 +0530

    People Page UI fixes

--- chesseract-fe/src/components/utilities/Avatar.tsx
+++ chesseract-fe/src/components/utilities/Avatar.tsx
@@ -38,7 +38,9 @@ const Avatar: React.FC<AvatarProps> = ({
             className="object-cover w-full h-full"
           />
         ) : (
-            <FaUser size={size-10}/>
+            <div className="h-full w-full flex items-center justify-center text-xl bg-[var(--color-primary-200)]">
+              {username.charAt(0).toUpperCase()}
+            </div>
         )}
       </div>
       
